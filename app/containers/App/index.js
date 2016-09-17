4610c257821b507ba4b882317270403dcbaf4ccf
Jasper <jshumakerpruitt@gmail.com>
2016-09-17 17:38:07 +0700

    redirect from protected routes

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,7 +7,11 @@
  */
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 import Helmet from 'react-helmet';
+import { createStructuredSelector } from 'reselect';
+import { selectToken } from './selectors';
 
 // Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
 import 'sanitize.css/sanitize.css';
@@ -18,27 +22,53 @@ import Footer from 'components/Footer';
 import styles from './styles.css';
 
 //    <AppBar title={}
-function App(props) {
-  return (
-    <div>
-      <Helmet
-        titleTemplate="%s - HelloRobot"
-        defaultTitle="HelloRobot - Social App for Robots"
-        meta={[
-          { name: 'description', content: 'Social App for Robots' },
-        ]}
-      />
-      <Header />
-      <div className={styles.wrapper}>
-        {React.Children.toArray(props.children)}
+export class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  componentWillMount() {
+    const path = this.props.location.pathname;
+    const token = this.props.token;
+
+    if (token.length === 0 && !this.isPublic(path)) {
+      this.props.dispatch(push('/login'));
+    }
+  }
+
+  isPublic(path) {
+    return ['/', '/login'].find((p) => p === path);
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet
+          titleTemplate="%s - HelloRobot"
+          defaultTitle="HelloRobot - Social App for Robots"
+          meta={[
+            { name: 'description', content: 'Social App for Robots' },
+          ]}
+        />
+        <Header />
+        <div className={styles.wrapper}>
+          {React.Children.toArray(this.props.children)}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 App.propTypes = {
   children: React.PropTypes.node,
+  location: React.PropTypes.object.isRequired,
+  token: React.PropTypes.string.isRequired,
+  dispatch: React.PropTypes.func.isRequired,
 };
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  token: selectToken(),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatch,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -2,31 +2,42 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import App from '../index';
+import { App } from '../index';
 import Footer from 'components/Footer';
+import Header from 'components/Header';
 
 describe('<App />', () => {
+  let props = null;
+
+  beforeEach(() => {
+    props = {
+      dispatch: () => {},
+      token: '',
+      location: { pathname: '/' },
+    };
+  });
+
   it('should render the header', () => {
     const renderedComponent = shallow(
-      <App />
+      <App {...props} />
     );
-    expect(renderedComponent.find('Header').length).toEqual(1);
+    expect(renderedComponent.find(Header).length).toEqual(1);
+  });
+
+  it('should render the footer', () => {
+    const renderedComponent = shallow(
+      <App {...props} />
+    );
+    expect(renderedComponent.find(Footer).length).toEqual(1);
   });
 
   it('should render its children', () => {
     const children = (<h1>Test</h1>);
     const renderedComponent = shallow(
-      <App>
+      <App {...props} >
         {children}
       </App>
     );
     expect(renderedComponent.contains(children)).toEqual(true);
-  });
-
-  it('should render the footer', () => {
-    const renderedComponent = shallow(
-      <App />
-    );
-    expect(renderedComponent.find(Footer).length).toEqual(1);
   });
 });
